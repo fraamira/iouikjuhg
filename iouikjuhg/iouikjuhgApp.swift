0f5d0b104c989495658bd9d0fe27db355f5344ae
fraamira <92482168+fraamira@users.noreply.github.com>
2022-05-26 14:48:07 +0200

    Initial Commit

---
+++ iouikjuhg/iouikjuhgApp.swift
@@ -0,0 +1,17 @@
+//
+//  iouikjuhgApp.swift
+//  iouikjuhg
+//
+//  Created by Francesco Amirante on 26/05/22.
+//
+
+import SwiftUI
+
+@main
+struct iouikjuhgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
